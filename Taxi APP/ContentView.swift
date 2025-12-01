29703452ee2516e3748bd1179423c7e321ce6d64
Lucky <luckypc81@gmail.com>
2025-12-01 14:32:35 +0100

    Initial Commit

---
+++ Taxi APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Taxi APP
+//
+//  Created by pece carmine on 01.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Taxi APP/Taxi_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taxi_APPApp.swift
+//  Taxi APP
+//
+//  Created by pece carmine on 01.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Taxi_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
