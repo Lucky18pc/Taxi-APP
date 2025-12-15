76d81e2c6514d66c01dbf6fe3f7ac141852072a7
Lucky18pc <luckypc81@gmail.com>
2025-12-15 16:09:28 +0100

    TaxiApp

--- Taxi APP/ContentView.swift
+++ Taxi APP/ContentView.swift
@@ -7,18 +7,103 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
 
-#Preview {
-    ContentView()
-}
+
+
+Title
+Start
+Abholung in ca. 08Min.möglich
+profilbild
++
+Bild Auto
+Taxi
+Logo
+June 2024
+􀆊
+􀆉
+􀆊
+Zentrale Anrufen
+Taxi abbestellen
+Name
+sun
+mon
+tue
+wed
+thu
+fri
+sat
+Zahlung
+vom
+2
+3
+4
+5
+6
+7
+8
+Hintergundbild
+12,40
+Taxi betrieb
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+Google Maps
+Hintergrundbilder
+18,00 € Tarif
+mit
+1,50  € Trinkgeld   Gutschein
+Logo
+Bild
+23
+24
+25
+26
+27
+28
+29
+goole maps
+30
+5%
+10%
+15%
+Trinkgeld
+Trinkgeld
+Trinkgeld
+Ends
+8:00 AM
+Title
+June
+2024
+9:41 AM
+ZahlungsArt
+Gutschein
+Details
+Title
+8:00 AM
+AM
+PM
+5,00
+€
+Details
+Title
+June
+2024
+BITTE  HOLEN SIE  MICH HIER AB
+Details
+Kunde  Kalender
+mit  abholzeit
+
+Weiter
+BESTÄTIGEN
+Weiter zum Bezahlen
